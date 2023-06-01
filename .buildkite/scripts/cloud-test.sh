82520160aba7078141962cc099afbe380c42fb5c
alejandroesc <jaescobar.cell@gmail.com>
2023-05-31 22:06:49 -0400

    update precommit script

--- .buildkite/scripts/cloud-test.sh
+++ .buildkite/scripts/cloud-test.sh
@@ -4,6 +4,7 @@ set -xeuo pipefail
 
 set
 
+
 PATH="$(realpath .local/bin):${PATH}"
 bash -O extglob -c "rm -v charts/redpanda/ci/!(2)[0-9]-*"
 
@@ -11,8 +12,22 @@ envsubst < ./charts/redpanda/ci/21-eks-tiered-storage-with-creds-values.yaml.tpl
 
 ct install --config .github/ct.yaml --upgrade --skip-missing-values | sed 's/>>> /--- /'
 
-echo '--- testing that there is data in the s3 bucket'
-if (aws s3 ls "s3://${TEST_BUCKET}" --recursive --summarize | grep 'Total Objects: 0'); then
-    echo "0 Objects in the bucket. Cloud-storage failed."
-    exit 1
-fi
+
+
+aws() {
+    echo '--- testing that there is data in the s3 bucket'
+    if (aws s3 ls "s3://${TEST_BUCKET}" --recursive --summarize | grep 'Total Objects: 0'); then
+        echo "0 Objects in the bucket. Cloud-storage failed."
+        exit 1
+    fi
+}
+
+gke() {
+  echo '--- testing that there is data in the gcloud bucket'
+  if (gsutil ls -lR gs://${TEST_BUCKET} | tail -n 1 | grep 'TOTAL: 0 objects'); then
+        echo "0 objects in the bucket. Cloud-storage failed."
+        exit 1
+  fi
+}
+
+${CLOUD_PROVIDER}
---
+++ charts/redpanda/ci/22-gke-tiered-storage-with-creds-values.yaml.tpl
@@ -0,0 +1,9 @@
+storage:
+  tieredConfig:
+    cloud_storage_enabled: true
+    cloud_storage_api_endpoint: storage.googleapis.com
+    cloud_storage_credentials_source: config_file
+    cloud_storage_region: "US-WEST1"
+    cloud_storage_bucket: "${TEST_BUCKET}"
+    cloud_storage_segment_max_upload_interval_sec: 1
+license_key: "${REDPANDA_SAMPLE_LICENSE}"
