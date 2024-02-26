86dea2fa83582d5e4581575e51ac15cca686c9b2
Chris Seto <chriskseto@gmail.com>
2024-02-26 10:33:43 -0500

    move redpanda tests to charts/redpanda
    This commit moves the go based tests for the redpanda chart from
    `main_test.go` to `charts/redpanda` to better match developer
    expectations.
    
    It additionally adds in partial go structs for modeling `values.yaml`
    and implements an `rpk`-esque client to ease writing assertions against
    cluster configuration values and the like.

---
+++ charts/redpanda/chart_test.go
@@ -0,0 +1,112 @@
+package redpanda
+
+import (
+	"os"
+	"testing"
+
+	"github.com/redpanda-data/helm-charts/pkg/helm"
+	"github.com/redpanda-data/helm-charts/pkg/helm/helmtest"
+	"github.com/redpanda-data/helm-charts/pkg/kube"
+	"github.com/redpanda-data/helm-charts/pkg/testutil"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TieredStorageStatic(t *testing.T) Values {
+	if os.Getenv("REDPANDA_LICENSE") == "" {
+		t.Skipf("$REDPANDA_LICENSE is not set")
+	}
+
+	return Values{
+		Enterprise: &Enterprise{
+			License: os.Getenv("REDPANDA_LICENSE"),
+		},
+		Storage: &Storage{
+			Tiered: &Tiered{
+				Config: map[string]any{
+					"cloud_storage_enabled":    true,
+					"cloud_storage_region":     "static-region",
+					"cloud_storage_bucket":     "static-bucket",
+					"cloud_storage_access_key": "static-access-key",
+					"cloud_storage_secret_key": "static-secret-key",
+				},
+			},
+		},
+	}
+}
+
+func TieredStorageSecret(namespace string) corev1.Secret {
+	return corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: "tiered-storage-",
+			Namespace:    namespace,
+		},
+		Data: map[string][]byte{
+			"access": []byte("from-secret-access-key"),
+			"secret": []byte("from-secret-secret-key"),
+		},
+	}
+}
+
+func TieredStorageSecretRefs(t *testing.T, secret *corev1.Secret) Values {
+	if os.Getenv("REDPANDA_LICENSE") == "" {
+		t.Skipf("$REDPANDA_LICENSE is not set")
+	}
+
+	return Values{
+		Enterprise: &Enterprise{
+			License: os.Getenv("REDPANDA_LICENSE"),
+		},
+		Storage: &Storage{
+			Tiered: &Tiered{
+				CredentialsSecretRef: &TieredStorageCredentials{
+					AccessKey: &SecretRef{Name: secret.Name, Key: "access"},
+					SecretKey: &SecretRef{Name: secret.Name, Key: "secret"},
+				},
+				Config: map[string]any{
+					"cloud_storage_enabled": true,
+					"cloud_storage_region":  "a-region",
+					"cloud_storage_bucket":  "a-bucket",
+				},
+			},
+		},
+	}
+}
+
+func TestChart(t *testing.T) {
+	if testing.Short() {
+		t.Skipf("Skipping log running test...")
+	}
+
+	env := helmtest.Setup(t).Namespaced(t)
+
+	t.Run("tiered-storage-secrets", func(t *testing.T) {
+		ctx := testutil.Context(t)
+
+		credsSecret, err := kube.Create(ctx, env.Ctl(), TieredStorageSecret(env.Namespace()))
+		require.NoError(t, err)
+
+		rpRelease := env.Install(".", helm.InstallOptions{})
+
+		rpk := Client{Ctl: env.Ctl(), Release: &rpRelease}
+
+		config, err := rpk.ClusterConfig(ctx)
+		require.NoError(t, err)
+		require.Equal(t, false, config["cloud_storage_enabled"])
+
+		rpRelease = env.Upgrade(".", rpRelease, helm.UpgradeOptions{Values: TieredStorageStatic(t)})
+
+		config, err = rpk.ClusterConfig(ctx)
+		require.NoError(t, err)
+		require.Equal(t, true, config["cloud_storage_enabled"])
+		require.Equal(t, "static-access-key", config["cloud_storage_access_key"])
+
+		rpRelease = env.Upgrade(".", rpRelease, helm.UpgradeOptions{Values: TieredStorageSecretRefs(t, credsSecret)})
+
+		config, err = rpk.ClusterConfig(ctx)
+		require.NoError(t, err)
+		require.Equal(t, true, config["cloud_storage_enabled"])
+		require.Equal(t, "from-secret-access-key", config["cloud_storage_access_key"])
+	})
+}
---
+++ charts/redpanda/client.go
@@ -0,0 +1,46 @@
+package redpanda
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+
+	"github.com/redpanda-data/helm-charts/pkg/helm"
+	"github.com/redpanda-data/helm-charts/pkg/kube"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/yaml"
+)
+
+type Client struct {
+	Ctl     *kube.Ctl
+	Release *helm.Release
+}
+
+func (c *Client) getStsPod(ctx context.Context, ordinal int) (*corev1.Pod, error) {
+	return kube.Get[corev1.Pod](ctx, c.Ctl, kube.ObjectKey{
+		Name:      fmt.Sprintf("%s-%d", c.Release.Name, ordinal),
+		Namespace: c.Release.Namespace,
+	})
+}
+
+func (c *Client) ClusterConfig(ctx context.Context) (ClusterConfig, error) {
+	pod, err := c.getStsPod(ctx, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	var out bytes.Buffer
+	if err := c.Ctl.Exec(ctx, pod, kube.ExecOptions{
+		Command: []string{"bash", "-c", `rpk cluster config export -f /dev/stderr`},
+		Stderr:  &out,
+	}); err != nil {
+		return nil, err
+	}
+
+	var config map[string]any
+	if err := yaml.Unmarshal(out.Bytes(), &config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
