0f628c464809c58f582d65140951a2d36a8be221
Chris Seto <chriskseto@gmail.com>
2024-05-30 14:56:59 -0400

    gotohelm: fix namespacing and function name overrides
    Prior to this commit, there were subtle bugs with both package namespacing and
    function name overriding that could result in template eval time errors.
    
    This was due to directives only being parsed at the top loop of pkg.Syntax and
    never being associated with objects later on. The ideal solution would be to
    factor out a dedicated struct and cache for accessing directive information but
    a temporary solution is being put in place for now.
    
    The core issue was primarily only noticeable when calling bootstrap functions
    from other bootstrap functions but a latent issue with name override also
    existed. This commit adds a namespace and function naming cache that lazily
    evaluate and cache their results that is now used in place of manually
    constructing names.

--- pkg/gotohelm/testdata/src/example/directives/directives.rewritten.go
+++ pkg/gotohelm/testdata/src/example/directives/directives.rewritten.go
@@ -1,6 +1,10 @@
 //go:build rewrites
+// +gotohelm:namespace=_directives
 package directives
 
 func Directives() bool {
+	// Calling Noop does nothing but asserts that it's referenced correctly
+	// with the correct namespacing.
+	Noop()
 	return true
 }
