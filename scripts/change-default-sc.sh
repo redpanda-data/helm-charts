b23c0ea2b97c88af92ef1c32b2452ac2b4d19c35
Chris Seto <chriskseto@gmail.com>
2024-11-25 09:50:57 -0500

    *: clear out unused buildkite configs
    This commit removes unused builtkite configurations, taskfile targets, and cAPI
    specifications.
    
    K8S-429

--- scripts/change-default-sc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-# Simple approach for now
-kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}' || true
-kubectl patch storageclass standard -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}' ||  true
